aa4ba8f0d9063b11360e3a1571d17a20bcc3a141
Una Stankovic <una_stankovic@yahoo.com>
2016-11-26 18:00:24 +0100

    jmbg field changed to varchar. AI options added.

--- skript1.sql
+++ skript1.sql
@@ -18,7 +18,7 @@ USE `ModnaRevija` ;
 -- Table `ModnaRevija`.`Ucesnici`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Ucesnici` (
-  `jmbg` INT NOT NULL,
+  `jmbg` VARCHAR(13) NOT NULL,
   `ime` VARCHAR(45) NOT NULL,
   `prezime` VARCHAR(45) NOT NULL,
   `adresa` VARCHAR(45) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Dizajner` (
   `pib` INT NOT NULL,
   `naziv firme` VARCHAR(45) NOT NULL,
   `adresa ateljea` VARCHAR(45) NOT NULL,
-  `Ucesnici_jmbg` INT NOT NULL,
+  `Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`Ucesnici_jmbg`),
   CONSTRAINT `fk_Dizajner_Ucesnici`
     FOREIGN KEY (`Ucesnici_jmbg`)
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Model` (
   `tezina` DOUBLE NOT NULL,
   `obim` DOUBLE NOT NULL,
   `visina` DOUBLE NOT NULL,
-  `Ucesnici_jmbg` INT NOT NULL,
+  `Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`Ucesnici_jmbg`),
   CONSTRAINT `fk_Model_Ucesnici1`
     FOREIGN KEY (`Ucesnici_jmbg`)
@@ -66,7 +66,7 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Pomocno osoblje`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Pomocno osoblje` (
-  `Ucesnici_jmbg` INT NOT NULL,
+  `Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`Ucesnici_jmbg`),
   CONSTRAINT `fk_Pomocno osoblje_Ucesnici1`
     FOREIGN KEY (`Ucesnici_jmbg`)
@@ -80,8 +80,8 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Angazuje`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Angazuje` (
-  `Dizajner_Ucesnici_jmbg` INT NOT NULL,
-  `Pomocno osoblje_Ucesnici_jmbg` INT NOT NULL,
+  `Dizajner_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
+  `Pomocno osoblje_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`Dizajner_Ucesnici_jmbg`, `Pomocno osoblje_Ucesnici_jmbg`),
   INDEX `fk_Zaposljava_Pomocno osoblje1_idx` (`Pomocno osoblje_Ucesnici_jmbg` ASC),
   CONSTRAINT `fk_Angazuje_Dizajner1`
@@ -101,9 +101,9 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Sredjuje`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Sredjuje` (
-  `Angazuje_Dizajner_Ucesnici_jmbg` INT NOT NULL,
-  `Angazuje_Pomocno osoblje_Ucesnici_jmbg` INT NOT NULL,
-  `Model_Ucesnici_jmbg` INT NOT NULL,
+  `Angazuje_Dizajner_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
+  `Angazuje_Pomocno osoblje_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
+  `Model_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`Angazuje_Dizajner_Ucesnici_jmbg`, `Angazuje_Pomocno osoblje_Ucesnici_jmbg`, `Model_Ucesnici_jmbg`),
   INDEX `fk_Sredjuje_Model1_idx` (`Model_Ucesnici_jmbg` ASC),
   CONSTRAINT `fk_Sredjuje_Zaposljava1`
@@ -123,8 +123,8 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Unajmljuje`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Unajmljuje` (
-  `Dizajner_Ucesnici_jmbg` INT NOT NULL,
-  `Model_Ucesnici_jmbg` INT NOT NULL,
+  `Dizajner_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
+  `Model_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   `Honorar` DOUBLE NOT NULL,
   PRIMARY KEY (`Dizajner_Ucesnici_jmbg`, `Model_Ucesnici_jmbg`),
   INDEX `fk_Unajmljuje_Model1_idx` (`Model_Ucesnici_jmbg` ASC),
@@ -145,7 +145,7 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Kolekcija`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Kolekcija` (
-  `idKolekcije` INT NOT NULL,
+  `idKolekcije` INT NOT NULL AUTO_INCREMENT,
   `sezona` VARCHAR(45) NOT NULL,
   `godina` YEAR NOT NULL,
   `naziv` VARCHAR(45) NOT NULL,
@@ -158,7 +158,7 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Kreira`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Kreira` (
-  `Dizajner_Ucesnici_jmbg` INT NOT NULL,
+  `Dizajner_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   `Kolekcija_idKolekcije` INT NOT NULL,
   PRIMARY KEY (`Dizajner_Ucesnici_jmbg`, `Kolekcija_idKolekcije`),
   INDEX `fk_Kreira_Kolekcija1_idx` (`Kolekcija_idKolekcije` ASC),
@@ -179,12 +179,12 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Haljina`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Haljina` (
-  `idHaljina` INT NOT NULL,
+  `idHaljina` INT NOT NULL AUTO_INCREMENT,
   `materijal` VARCHAR(45) NOT NULL,
   `duzina(cm)` DOUBLE NOT NULL,
   `tip` VARCHAR(45) NOT NULL,
   `Kolekcija_idKolekcije` INT NOT NULL,
-  `Model_Ucesnici_jmbg` INT NOT NULL,
+  `Model_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`idHaljina`),
   INDEX `fk_Haljina_Kolekcija1_idx` (`Kolekcija_idKolekcije` ASC),
   INDEX `fk_Haljina_Model1_idx` (`Model_Ucesnici_jmbg` ASC),
@@ -216,7 +216,7 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Modna revija`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Modna revija` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `datum` DATE NOT NULL,
   `vreme` TIME NOT NULL,
   `sezona` VARCHAR(45) NOT NULL,
@@ -238,7 +238,7 @@ CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Organizuje` (
   `datum` DATE NOT NULL,
   `vreme` TIME NOT NULL,
   `Modna revija_id` INT NOT NULL,
-  `Dizajner_Ucesnici_jmbg` INT NOT NULL,
+  `Dizajner_Ucesnici_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`Modna revija_id`, `Dizajner_Ucesnici_jmbg`),
   INDEX `fk_Organizuje_Dizajner1_idx` (`Dizajner_Ucesnici_jmbg` ASC),
   CONSTRAINT `fk_Organizuje_Modna revija1`
@@ -258,7 +258,7 @@ ENGINE = InnoDB;
 -- Table `ModnaRevija`.`Gost`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Gost` (
-  `jmbg` INT NOT NULL,
+  `jmbg` VARCHAR(13) NOT NULL,
   `ime` VARCHAR(45) NOT NULL,
   `prezime` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`jmbg`))
@@ -272,7 +272,7 @@ CREATE TABLE IF NOT EXISTS `ModnaRevija`.`Pozivnica` (
   `tekst` VARCHAR(100) NOT NULL,
   `broj stolice` INT NOT NULL,
   `Modna revija_id` INT NOT NULL,
-  `Gost_jmbg` INT NOT NULL,
+  `Gost_jmbg` VARCHAR(13) NOT NULL,
   PRIMARY KEY (`Modna revija_id`, `broj stolice`),
   INDEX `fk_Pozivnica_Gost1_idx` (`Gost_jmbg` ASC),
   CONSTRAINT `fk_Pozivnica_Modna revija1`
