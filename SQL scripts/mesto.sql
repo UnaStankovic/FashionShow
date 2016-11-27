c679f27007244e7f9e1ecc7cbffb2d441fb67df6
Una Stankovic <una_stankovic@yahoo.com>
2016-11-27 23:59:23 +0100

    Major project changes at all levels.

--- SQL scripts/mesto.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `Mesto odrzavanja`(naziv, ptt, adresa) VALUES
-('Beograd', '11000', 'Pariske komune 31'),
-('Novi Sad', '21000', 'General Zdanova 17'),
-('Cacak', '32000', 'Milesevska 83'),
-('Kraljevo', '36000', 'Save Kovacevica 145'),
-('Krusevac', '37000', 'Galilejeva 126'),
-('Subotica', '24000', 'Brace Jugovic 7'),
-('Sabac', '15000', 'Simina 6')
