a4b021945b42ed97b783d3e64e310d3192f4e41f
Una Stankovic <una_stankovic@yahoo.com>
2016-11-28 03:24:12 +0100

    Adding scripts.

---
+++ SQL script/mesto.sql
@@ -0,0 +1,8 @@
+INSERT INTO `Mesto_odrzavanja`(naziv, ptt, adresa) VALUES
+('Beograd', 11000, 'Pariske komune 33'),
+('Novi Sad', 21000, 'General Zdanova 93'),
+('Cacak', 32000, 'Milesevska 17'),
+('Kraljevo', 36000, 'Save Kovacevica 26'),
+('Krusevac', 37000, 'Galilejeva 52'),
+('Subotica', 24000, 'Brace Jugovic 31'),
+('Sabac', 15000, 'Simina 54')
